e962d2410bda0f4dfc3f0348ffc803931c9f95a6
Sergej Samsonow <sergej.samsonow.public@googlemail.com>
2018-10-14 07:51:04 +0200

    Add message msg and functions export

--- core/lib/message.sh
+++ core/lib/message.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 BOLD="\e[1m"
 COLOR_RED="\e[31m"
+COLOR_GREEN="\e[92m"
 COLOR_END="\e[0m"
+
 function error_msg {
     ERROR_MSG=$1
     if [ -z "$ERROR_MSG" ]
@@ -9,4 +11,9 @@ function error_msg {
         ERROR_MSG="Error with undefined error message is occured"
     fi
     echo -e "${COLOR_RED}${BOLD}ERROR:${COLOR_END} $ERROR_MSG exit(1)"
+}
+
+function msg {
+    MSG=$1
+    echo -e "  ${COLOR_GREEN}${BOLD}*${COLOR_END}  $MSG"
 }
